7f22a0d3933105d277a7ff3e7ec4bba523a8b9c4
Bronson Spies <spiesbronson@gmail.com>
2020-07-06 18:23:09 -0600

    Objective2Lab2 Completed

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Bronson Spies";
+    int age = 23;
+    String state = "Colorado";
+
+    System.out.println(name + " " + age + " " + state);
+  }
+}
