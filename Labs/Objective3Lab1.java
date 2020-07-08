95984b34216747abbb81ed03b606ed8a567bba5d
Bronson Spies <spiesbronson@gmail.com>
2020-07-07 21:23:13 -0600

    Objective3Lab1 completed

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1{
+  public static void main(String[] args) {
+    double num1 = 17;
+    double num2 = 12;
+    double num3 = 4;
+    double average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
