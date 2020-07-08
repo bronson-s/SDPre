db57667503e4171238525bbc08437c7a76ccfe6a
Bronson Spies <spiesbronson@gmail.com>
2020-07-08 10:51:54 -0600

    Objective3Lab2 completed

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2{
+  public static void main(String[] args) {
+    double side1 = (10 * 10);
+    double side2 = (6 * 6);
+    double hypotenuse = side1 + side2;
+    System.out.println(Math.sqrt(hypotenuse));
+
+  }
+}
