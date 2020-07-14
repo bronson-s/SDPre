cd6c19ecfdd2fe4b78d366d69d29da132d264a04
Bronson Spies <spiesbronson@gmail.com>
2020-07-14 16:03:11 -0600

    Complete Objective5Lab4

---
+++ Labs/Objective5Lab4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Objective5Lab4{
+  public static void main(String[] args) {
+
+Scanner kb = new Scanner(System.in);
+int num;
+
+    System.out.println("Please enter a number:");
+    num = kb.nextInt();
+kb.close();
+
+   if(num % 2 == 0){
+     System.out.println("The number is even");
+   }
+   else if(num % 2 > 0){
+     System.out.println("The number is odd");
+   }
+
+  }
+}
