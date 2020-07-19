7dd16bfe5f681d93732d37504696c6a3686162b8
Bronson Spies <spiesbronson@gmail.com>
2020-07-19 11:33:54 -0600

    Complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,16 @@
+
+
+public class Objective8Lab1{
+  public static void main(String[] args) {
+
+for (int i=0; i<=10; i += 1){
+  System.out.println(i);
+}
+for (int k=10; k>=0; k -= 1){
+  System.out.println(k);
+}
+
+
+
+ }
+}
